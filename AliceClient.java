784d58aee10b7ab8ce0bc81a7bd50c07444c3b23
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-11-03 00:02:57 -0700

    comments, input, and names

--- AliceClient.java
+++ AliceClient.java
@@ -1,45 +1,59 @@
+/**
+* @author Dustin Hurst
+* CS 3250
+* Assignemtn 4
+* Client side of the program that will speak to the Alice server bot and ask her questions about UVU and get a reply from her
+**/
+
 import java.io.*;
 import java.net.*;
 
 public class AliceClient {
-    public static void main(String[] args) throws IOException {
-
-        String serverHostname = new String ("127.0.0.1");
+    	public static void main(String[] args) throws IOException {
+	
+	//check proper arguments are entered
+	if(args.length != 1){
+		System.out.println("Usage: java AliceClient <ip address>");
+		System.exit(1);
+	}
+        String serverHostname = new String (args[0]);
 
         int port = 9000;
 
-        Socket echoSocket = null;
+        Socket socket = null;
         PrintWriter out = null;
         BufferedReader in = null;
-
+	
+	//Connect to server
         try {
-	    echoSocket = new Socket(serverHostname, port);
-            out = new PrintWriter(echoSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                                        echoSocket.getInputStream()));
+	   	socket = new Socket(serverHostname, port);
+           	out = new PrintWriter(socket.getOutputStream(), true);
+           	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + serverHostname);
-            System.exit(1);
+        	System.err.println("Don't know about host: " + serverHostname);
+           	System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for "
-                               + "the connection to: " + serverHostname);
-            System.exit(1);
+           	System.err.println("Couldn't get I/O for " + "the connection to: " + serverHostname);
+           	System.exit(1);
         }
 
-	BufferedReader stdIn = new BufferedReader(
-                                   new InputStreamReader(System.in));
+	BufferedReader stdIn = new BufferedReader( new InputStreamReader(System.in));
 	String userInput;
 
-        System.out.print ("input: ");
+	//Interaction with Alice begins here
+	System.out.println("You are now connected to Alice. How can I help?");
+        System.out.print ("You: ");
 	while ((userInput = stdIn.readLine()) != null) {
-	    out.println(userInput);
-	    System.out.println("echo: " + in.readLine());
-            System.out.print ("input: ");
+		out.println(userInput);
+	    	System.out.println("Alice: " + in.readLine());
+           	System.out.print ("You: ");
+		if(userInput.equals("bye") || userInput.equals("goodbye"))
+			break;
 	}
 
 	out.close();
 	in.close();
 	stdIn.close();
-	echoSocket.close();
+	socket.close();
     }
 }
--- AliceServer.java
+++ AliceServer.java
@@ -1,55 +1,57 @@
+/**
+* @author Dustin Hurst
+* CS 3250
+* Assignment 4
+* The Alice Server Bot that will make simple dynamic replies to the clients messages (questions)
+**/
+
 import java.net.*; 
 import java.io.*; 
 
 public class AliceServer{
  
- public static void main(String[] args) throws IOException 
-   { 
-    ServerSocket serverSocket = null; 
+	public static void main(String[] args) throws IOException { 
+    
+	ServerSocket serverSocket = null; 
 
-    try { 
-         serverSocket = new ServerSocket(9000); 
+    	try { 
+         	serverSocket = new ServerSocket(9000); 
         } 
-    catch (IOException e) 
+    	catch (IOException e) 
         { 
-         System.err.println("Could not listen on port: 9000."); 
-         System.exit(1); 
+         	System.err.println("Could not listen on port: 9000."); 
+         	System.exit(1); 
         } 
 
-    Socket clientSocket = null; 
-    System.out.println ("Waiting for connection.....");
+    	Socket clientSocket = null; 
+    	System.out.println ("Waiting for connection.....");
 
-    try { 
-         clientSocket = serverSocket.accept(); 
+    	try { 
+         	clientSocket = serverSocket.accept(); 
         } 
-    catch (IOException e) 
-        { 
-         System.err.println("Accept failed."); 
-         System.exit(1); 
-        } 
-
-    System.out.println ("Connection successful");
-    System.out.println ("Waiting for input.....");
-
-    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                                      true); 
-    BufferedReader in = new BufferedReader( 
-            new InputStreamReader( clientSocket.getInputStream())); 
-
-    String inputLine; 
-
-    while ((inputLine = in.readLine()) != null) 
-        { 
-         System.out.println ("Server: " + inputLine); 
-         out.println(inputLine); 
-
-         if (inputLine.equals("Bye.")) 
-             break; 
+    	catch (IOException e) { 
+         	System.err.println("Accept failed."); 
+         	System.exit(1); 
         } 
 
-    out.close(); 
-    in.close(); 
-    clientSocket.close(); 
-    serverSocket.close(); 
-   } 
+   	 System.out.println ("You are now connected to Alice");
+
+    	PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true); 
+    	BufferedReader in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream())); 
+
+    	String inputLine; 
+	
+	while ((inputLine = in.readLine()) != null) 
+  	{ 
+        	System.out.println ("Alice: " + inputLine); 
+        	out.println(inputLine); 
+        } 
+	/*
+    	out.close(); 
+    	in.close(); 
+    	clientSocket.close(); 
+    	serverSocket.close(); 
+	*/
+   	} 
+
 } 
