70d4ecd1fc767d0e087f5bcb32f9c28024d56597
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-11-02 18:22:11 -0700

    First commit

---
+++ AliceClient.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+
+public class AliceClient {
+    public static void main(String[] args) throws IOException {
+
+        String serverHostname = new String ("127.0.0.1");
+
+        int port = 9000;
+
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+	    echoSocket = new Socket(serverHostname, port);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                                        echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + serverHostname);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                               + "the connection to: " + serverHostname);
+            System.exit(1);
+        }
+
+	BufferedReader stdIn = new BufferedReader(
+                                   new InputStreamReader(System.in));
+	String userInput;
+
+        System.out.print ("input: ");
+	while ((userInput = stdIn.readLine()) != null) {
+	    out.println(userInput);
+	    System.out.println("echo: " + in.readLine());
+            System.out.print ("input: ");
+	}
+
+	out.close();
+	in.close();
+	stdIn.close();
+	echoSocket.close();
+    }
+}
