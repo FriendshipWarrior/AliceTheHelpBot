ee90f8996cbc144a96e2490304ad860e39567f87
Derek Strasters <paracite.org@gmail.com>
2015-11-09 00:15:13 -0700

    fixed duplicate message issue

--- src/AliceClient.java
+++ src/AliceClient.java
@@ -1,3 +1,4 @@
+
 /**
 * @author Dustin Hurst
 * CS 3250
@@ -9,51 +10,61 @@ import java.io.*;
 import java.net.*;
 
 public class AliceClient {
-    	public static void main(String[] args) throws IOException {
+
+	private static final String EXIT_CODE = "Alice: @@@@SERVER_EXIT";
 	
-	//check proper arguments are entered
-	if(args.length != 1){
-		System.out.println("Usage: java AliceClient <ip address>");
-		System.exit(1);
+	public static void main(String[] args) throws IOException {
+
+		// check proper arguments are entered
+		if (args.length != 1) {
+			System.out.println("Usage: java AliceClient <ip address>");
+			System.exit(1);
+		}
+		String serverHostname = new String(args[0]);
+
+		int port = 9000;
+
+		Socket socket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+
+		// Connect to server
+		try {
+			socket = new Socket(serverHostname, port);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: " + serverHostname);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for " + "the connection to: " + serverHostname);
+			System.exit(1);
+		}
+
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String userInput;
+
+		// Interaction with Alice begins here
+		System.out.println("You are now connected to Alice. How can I help?");
+		System.out.print("You: ");
+		while ((userInput = stdIn.readLine()) != null) {
+			out.println(userInput);
+
+			String incoming = in.readLine();
+
+			if (incoming.equals(EXIT_CODE)) {
+				System.out.printf("Allice: Goodbye! \n");
+				break;
+			}
+			
+			System.out.println(incoming);
+
+			System.out.print("You: ");
+		}
+
+		out.close();
+		in.close();
+		stdIn.close();
+		socket.close();
 	}
-        String serverHostname = new String (args[0]);
-
-        int port = 9000;
-
-        Socket socket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-	
-	//Connect to server
-        try {
-	   	socket = new Socket(serverHostname, port);
-           	out = new PrintWriter(socket.getOutputStream(), true);
-           	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch (UnknownHostException e) {
-        	System.err.println("Don't know about host: " + serverHostname);
-           	System.exit(1);
-        } catch (IOException e) {
-           	System.err.println("Couldn't get I/O for " + "the connection to: " + serverHostname);
-           	System.exit(1);
-        }
-
-	BufferedReader stdIn = new BufferedReader( new InputStreamReader(System.in));
-	String userInput;
-
-	//Interaction with Alice begins here
-	System.out.println("You are now connected to Alice. How can I help?");
-        System.out.print ("You: ");
-	while ((userInput = stdIn.readLine()) != null) {
-		out.println(userInput);
-	    	System.out.println("Alice: " + in.readLine());
-           	System.out.print ("You: ");
-		if(userInput.equals("bye") || userInput.equals("goodbye"))
-			break;
-	}
-
-	out.close();
-	in.close();
-	stdIn.close();
-	socket.close();
-    }
 }
--- src/AliceServer.java
+++ src/AliceServer.java
@@ -1,3 +1,4 @@
+
 /**
 * @author Dustin Hurst
 * CS 3250
@@ -11,137 +12,151 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.io.*; 
+import java.io.*;
 
-public class AliceServer{
- 
-	public static final String EXIT_CODE = "@@@@SERVER_EXIT"; 
-	
-	public static void main(String[] args) throws IOException { 
-    
-	ServerSocket serverSocket = null; 
+public class AliceServer {
 
-    	try { 
-         	serverSocket = new ServerSocket(9000); 
-        } 
-    	catch (IOException e) 
-        { 
-         	System.err.println("Could not listen on port: 9000."); 
-         	System.exit(1); 
-        } 
+	private static final String EXIT_CODE = "@@@@SERVER_EXIT";
 
-    	Socket clientSocket = null; 
-    	System.out.println ("Waiting for connection.....");
+	private static Map<String, Factoid> factDB;
+	private static Map<String, List<Factoid>> invertDB;
 
-    	try { 
-         	clientSocket = serverSocket.accept(); 
-        } 
-    	catch (IOException e) { 
-         	System.err.println("Accept failed."); 
-         	System.exit(1); 
-        } 
+	public static void main(String[] args) throws IOException {
+		factDB = makeDatabase();
+		invertDB = getInvertDatabase(factDB);
 
-   	 System.out.println ("You are now connected to Alice");
+		while (true) {
+			ServerSocket serverSocket = null;
 
-    	PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true); 
-    	BufferedReader in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream())); 
+			try {
+				serverSocket = new ServerSocket(9000);
+			} catch (IOException e) {
+				System.err.println("Could not listen on port: 9000.");
+				System.exit(1);
+			}
 
-    	String inputLine; 
-	
-	while ((inputLine = in.readLine()) != null) 
-  	{ 
-			String userEcho = "You: " + inputLine;
-        	String botResponse = "Allice: " + getResponse(inputLine);
+			Socket clientSocket = null;
+			System.out.println("Waiting for connection.....");
+
+			try {
+				clientSocket = serverSocket.accept();
+			} catch (IOException e) {
+				System.err.println("Accept failed.");
+				System.exit(1);
+			}
+
+			System.out.println("You are now connected to Alice");
+
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			String inputLine;
+			String botResponse = "";
+			String formatedResponse = "";
+
+			while ((inputLine = in.readLine()) != null && botResponse != EXIT_CODE) {
+				String userEcho = "You: " + inputLine;
+
+				botResponse = getResponse(inputLine);
+				formatedResponse = "Alice: " + botResponse;
+
+				if (botResponse != EXIT_CODE) {
+					System.out.print(userEcho);
+					System.out.println(formatedResponse);
+					out.println(formatedResponse);
+				} else {
+					System.out.printf("Allice: Goodbye! \n");
+				}
+			}
 			
-			System.out.println (userEcho); 
-        	out.println(inputLine);
-        	
-        	System.out.println (botResponse);
-        	out.println(botResponse);
-        } 
-	/*
-    	out.close(); 
-    	in.close(); 
-    	clientSocket.close(); 
-    	serverSocket.close(); 
-	*/
-   	} 
+			out.close();
+			in.close();
+			clientSocket.close();
+			serverSocket.close();
+		}
+	}
 
-	
 	/**
 	 * Convert a String line into an String array of the space delimited words
-	**/
+	 **/
 	private static String[] lineToWords(String line) {
 		String[] words = line.split("\\s+");
-		
+
 		for (int i = 0; i < words.length; i++) {
-		    words[i] = words[i].replaceAll("[^\\w]", "");
+			words[i] = words[i].replaceAll("[^\\w]", "");
 		}
-		
+
 		return words;
 	}
-	
+
 	/**
-	 *  Returns the most plural Factoid in a list of Factoids
-	**/
+	 * Returns the most plural Factoid in a list of Factoids
+	 **/
 	public static Factoid mostCommonElement(List<Factoid> list) {
-	    Map<Factoid, Integer> elementCount = new HashMap<>();
+		Map<Factoid, Integer> elementCount = new HashMap<>();
 
-	    for (Factoid factoid : list) {
-	        Integer val = elementCount.get(factoid);
-	        elementCount.put(factoid, val == null ? 1 : val + 1);
-	    }
+		for (Factoid factoid : list) {
+			Integer val = elementCount.get(factoid);
+			elementCount.put(factoid, val == null ? 1 : val + 1);
+		}
 
-	    Entry<Factoid, Integer>max = null;
+		Entry<Factoid, Integer> max = null;
 
-	    for (Map.Entry<Factoid, Integer> factoidEntry : elementCount.entrySet()) {
-	        if (max == null || factoidEntry.getValue() > max.getValue())
-	            max = factoidEntry;
-	    }
+		for (Map.Entry<Factoid, Integer> factoidEntry : elementCount.entrySet()) {
+			if (max == null || factoidEntry.getValue() > max.getValue())
+				max = factoidEntry;
+		}
 
-	    return max.getKey();
+		return max.getKey();
 	}
-	
+
 	/**
 	 * 
-	 * Use @param inputString to pass in a grammatical sentance 
+	 * Use @param inputString to pass in a grammatical sentance
+	 * 
 	 * @return
 	 */
 	public static String getResponse(String inputString) {
 		String response = "";
-		// Make the input line into an array of word Strings, and format each to lower case with only alpha numeric
+		// Make the input line into an array of word Strings, and format each to
+		// lower case with only alpha numeric
 		String[] inputWords = lineToWords(inputString.toLowerCase().replaceAll("[^A-Za-z0-9 ]", "")); // Apply some formatting
 		List<Factoid> potentialFactiods = new ArrayList<Factoid>();
 		List<Factoid> factoidKeysTemp = new ArrayList<Factoid>();
-		Factoid winner;
-		
+
 		// Iterate throught each word in the input string
 		for (String word : inputWords) {
 			factoidKeysTemp.clear();
-			// Search the inverse database for that value and set temp variable to array of Factiods containing that keyword 
-			factoidKeysTemp = FactoidData.invertDB.get(word);
-			if (factoidKeysTemp != null) {
-				// Assert that temp is not null, and add the list of factoids to potential matches list
-				potentialFactiods.addAll(factoidKeysTemp);
+			// Search the inverse database for that value and set temp variable
+			// to array of Factiods containing that keyword
+			if (invertDB.containsKey(word)) {
+				factoidKeysTemp = invertDB.get(word);
+				if (factoidKeysTemp != null) {
+					// Assert that temp is not null, and add the list of
+					// factoids to potential matches list
+					potentialFactiods.addAll(factoidKeysTemp);
+				}
 			}
 		}
-		
-		//Which ever Factoid appears most in the list of potentials is selected
-		winner = mostCommonElement(potentialFactiods);
-		
-		return winner!=null? winner.getresponse() : "I'm affraid I don't know how to respond to that...";
+
+		if (potentialFactiods.isEmpty()) {
+			// Which ever Factoid appears most in the list of potentials is
+			// selected
+			response = factDB.get("i dont know").getresponse();
+		} else {
+			response = mostCommonElement(potentialFactiods).getresponse();
+		}
+
+		return response;
 	}
-	
+
 	/*----------------  inner classes  --------------------*/
-	
-	
-	
+
 	public static class Factoid {
-		
+
 		private String[] keywords;
 		private String response;
-		
-		
+
 		public Factoid(String[] keywords, String response) {
 			this.keywords = keywords;
 			this.response = response;
@@ -155,7 +170,8 @@ public class AliceServer{
 		}
 
 		/**
-		 * @param keywords the keywords to set
+		 * @param keywords
+		 *            the keywords to set
 		 */
 		public void setKeywords(String[] keywords) {
 			this.keywords = keywords;
@@ -169,92 +185,95 @@ public class AliceServer{
 		}
 
 		/**
-		 * @param response the response to set
+		 * @param response
+		 *            the response to set
 		 */
 		public void setresponse(String response) {
 			this.response = response;
 		}
 	}
-	
-	public static class FactoidData {
 
-		/**
-		 * The main database of Factoids.
-		 * 
-		 * A short description of the answer is used as the key, and Factoid object is the value.
-		 * The Factiod objects are instantiated on entry with an array of keywords to trigger the response
-		 * as well as the response for the factiod
-		 * 
-		 * LOWER CASE ONLY
-		 * DO NOT USE APPOSTROPHE
-		*/
-		public static Map<String, Factoid> factDB;
-		static { 
-			factDB = new HashMap<String, Factoid>();
-			
-			//TODO: populate this list
-			factDB.put("canvas web address", new Factoid( 
-					new String[] {
-					    "internet",
-						"web",
-						"online",
-						"canvas",
-						"student", 
-						"portal"
-						},
-					"The Canvas student portal may be reached at web adress INSERT HERE"));
-			
-			factDB.put("chair of accounting", new Factoid( 
-					new String[] {
-					    "stanley",
-						"jenne",
-						"accounting",
-						"name",
-						"department",
-						"chair",
-						"who"
-						},
-					"The chair of accounting is Professor Stanley E Jenne Ph.D."));
-			
-			factDB.put("exit code", new Factoid( 
-					new String[] {
-					    "exit",
-						"good",
-						"bye",
-						"bye",
-						"see",
-						"you",
-						"later",
-						"peace",
-						"out",
-						"goodbye"
-						},
-					EXIT_CODE));
-			
-		}
-		
-		
-		/**
-		 *  This is an inverted mapping from the Factiod database.
-		 *  Since it is not one to one, the Factiod keys are stored within a List.
-		 */
-		public static Map<String, List<Factoid>> invertDB;
-		static {
-			// Iterate all entries in the Factoid database
-			for (Map.Entry<String, Factoid> entry : factDB.entrySet()) {
-				// Iterate all keywords for each Factoid: get factoid from entry then get keyword array from factiod and iterate
-				for (String keyword : entry.getValue().getKeywords()) {
-					// Check if keyword is in the inverted database already
-					if (!invertDB.containsKey(entry.getValue())){
-						// If keyword is not yet in the inverted database, 
-						// put the keyword entry in as the map key and make new a List for the value
-						invertDB.put(keyword, new ArrayList<Factoid>());
-					} 
-					// Add the Factiod object that contains the keyword to list of Factoids for this keyword
-					invertDB.get(keyword).add(entry.getValue());
+	/**
+	 * The main database of Factoids.
+	 * 
+	 * A short description of the answer is used as the key, and Factoid object
+	 * is the value. The Factiod objects are instantiated on entry with an array
+	 * of keywords to trigger the response as well as the response for the
+	 * factiod
+	 * 
+	 * LOWER CASE ONLY DO NOT USE APPOSTROPHE
+	 */
+
+	private static Map<String, List<Factoid>> getInvertDatabase(Map<String, Factoid> DB) {
+		Map<String, List<Factoid>> invertDB = new HashMap<String, List<Factoid>>();
+
+		// Iterate all entries in the Factoid database
+		for (Map.Entry<String, Factoid> entry : DB.entrySet()) {
+			// Iterate all keywords for each Factoid: get factoid from entry
+			// then get keyword array from factiod and iterate
+			for (String keyword : entry.getValue().getKeywords()) {
+				// Check if keyword is in the inverted database already
+				if (!invertDB.containsKey(entry.getValue())) {
+					// If keyword is not yet in the inverted database,
+					// put the keyword entry in as the map key and make new a
+					// List for the value
+					invertDB.put(keyword, new ArrayList<Factoid>());
 				}
+				// Add the Factiod object that contains the keyword to list of
+				// Factoids for this keyword
+				invertDB.get(keyword).add(entry.getValue());
 			}
 		}
-	}
 
+		return invertDB;
+	}
+	
+	private static Map<String, Factoid> makeDatabase() { 
+		Map<String, Factoid> DB = new HashMap<String, Factoid>();
+		
+		//TODO: populate this list
+		DB.put("canvas web address", new Factoid( 
+				new String[] {
+				    "internet",
+					"web",
+					"online",
+					"canvas",
+					"student", 
+					"portal"
+					},
+				"The Canvas student portal may be reached at web adress INSERT HERE"));
+		
+		DB.put("chair of accounting", new Factoid( 
+				new String[] {
+				    "stanley",
+					"jenne",
+					"accounting",
+					"name",
+					"department",
+					"chair",
+					"who"
+					},
+				"The chair of accounting is Professor Stanley E Jenne Ph.D."));
+		
+		DB.put("exit code", new Factoid( 
+				new String[] {
+				    "exit",
+					"good",
+					"bye",
+					"bye",
+					"see",
+					"you",
+					"later",
+					"peace",
+					"out",
+					"goodbye"
+					},
+				EXIT_CODE));
+		
+		DB.put("i dont know", new Factoid(
+				new String[] {},
+				"I'm affaid I can't do that Dave..."));
+		
+		return DB;
+	}
 } 
